ca234207889981ad7e7479ed9a8d8d163a99bfdf
Damian Ho <damian_ho_xu_yang@yahoo.com>
2016-01-24 12:14:47 +0800

    Change port

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var app = express();
 require("./controllers/config.js")(app, express);
 app.use(require("./controllers/routes.js"));
 
-app.listen(process.env.PORT || 8080, process.env.IP || "127.0.0.1");
-console.info("Listening on port 8080.");
+app.listen(process.env.PORT || 5000, process.env.IP || "127.0.0.1");
+console.info("Listening on port " + (process.env.PORT || 5000) + ".");
